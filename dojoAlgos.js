72b2e0f0432b246c8cc730f26b58cbd733cae829
bghebrit <sghebrit@gmail.com>
2020-10-01 19:08:36 -0700

    new

---
+++ dojoAlgos.js
@@ -0,0 +1,36 @@
+// function printOneto255(){
+//     for(var x=1;x<256;x++){
+//         console.log(x)
+//     }
+// }
+
+// printOneto255()
+
+// function oddNums(){
+//     var i = 1;
+//     while(i < 1001){
+//         console.log(i);
+//         i=i+2;
+//     }
+
+// }
+
+// oddNums()
+
+
+// function oddSum(){
+//     var my_sum = 0;
+//     for(var i=1;i<5001;i=i+2){
+//         my_sum += i;
+//     }
+//     console.log(my_sum)
+// }
+
+// oddSum()
+
+function iterrate(arr){
+    for(var i=0;i<arr.length;i++)
+    console.log(arr[i]);
+}
+
+iterrate([1,3,5,7,9,13])
